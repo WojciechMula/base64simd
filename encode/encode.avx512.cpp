985f5869b815d49ad228108291191de719a4d36a
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-09-15 16:54:52 +0200

    encoding: AVX512F - loading data without gather

--- encode/encode.avx512.cpp
+++ encode/encode.avx512.cpp
@@ -2,6 +2,9 @@
 #include <x86intrin.h>
 
 #include <cstdint>
+#include "../debug_dump.cpp"
+
+//#define GATHER_ASSISTED_LOAD
 
 namespace base64 {
 
@@ -12,6 +15,7 @@ namespace base64 {
 
             uint8_t* out = output;
 
+#ifdef GATHER_ASSISTED_LOAD
             static const uint32_t input_offsets[16] = {
                  0*3,  1*3,  2*3,  3*3,
                  4*3,  5*3,  6*3,  7*3,
@@ -20,10 +24,23 @@ namespace base64 {
             };
 
             const __m512i input_gather = _mm512_loadu_si512(reinterpret_cast<const __m512i*>(input_offsets));
-
+#endif
             for (size_t i = 0; i < bytes; i += 4 * 12) {
                 // load bytes
+#ifdef GATHER_ASSISTED_LOAD
                 const __m512i in = _mm512_i32gather_epi32(input_gather, (const int*)(input + i), 1);
+#else
+                const __m512i tmp1 = _mm512_loadu_si512(input + i);
+                const __m512i tmp2 = _mm512_permutexvar_epi32(
+                    _mm512_set_epi32(11, 10, 10, 9, 8, 7, 7, 6, 5, 4, 4, 3, 2, 1, 1, 0),
+                    tmp1
+                );
+                const __m512i tmp3 = _mm512_mask_srli_epi64(tmp2, 0xaa, tmp2, 16);
+                const __m512i tmp4 = _mm512_slli_epi64(tmp3, 8);
+                const __m512i tmp5 = _mm512_ternarylogic_epi64(_mm512_set1_epi64(0x00ffffff), tmp4, tmp3, 0xac);
+
+                const __m512i in = tmp5;
+#endif // defined(GATHER_ASSISTED_LOAD)
 
                 const __m512i indices = unpack(in);
 
