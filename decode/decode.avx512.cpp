677fb966f58f205e97864eb943a68ad30b88673b
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-09-16 17:36:53 +0200

    Revert "decoding: AVX512F - saving data without scatter"
    This reverts commit 492d43b4a0b0687a92c6687831e59480f535db03.
    
    SSE nor AVX2 didn't help.

--- decode/decode.avx512.cpp
+++ decode/decode.avx512.cpp
@@ -4,23 +4,17 @@
 #include <immintrin.h>
 #include <x86intrin.h>
 
-
-//#define SCATTER_ASSISTED_STORE
-#define AVX2_ASSISTED_STORE
-//#define SSE_ASSISTED_STORE
-
 namespace base64 {
 
     namespace avx512 {
-#ifdef SCATTER_ASSISTED_STORE
+
         namespace precalc {
 
             static __m512i scatter_offsets;
         }
-#endif
 
         void initalize_decode() {
-#ifdef SCATTER_ASSISTED_STORE
+
             uint32_t lookup[16];
 
             for (int i=0; i < 16; i++) {
@@ -28,12 +22,7 @@ namespace base64 {
             }
 
             precalc::scatter_offsets = _mm512_loadu_si512(reinterpret_cast<__m512i*>(lookup));
-#endif
-
         }
-
-
-        static const uint8_t BIT_MERGE = 0xac;
 
 
         template <typename FN_LOOKUP, typename FN_PACK>
@@ -58,55 +47,8 @@ namespace base64 {
 
                 const __m512i packed = pack(values);
 
-#ifdef SCATTER_ASSISTED_STORE
                 _mm512_i32scatter_epi32(reinterpret_cast<int*>(out), precalc::scatter_offsets, packed, 1);
-#endif
 
-#ifdef SSE_ASSISTED_STORE
-                const __m128i dq0 = _mm512_extracti32x4_epi32(packed, 0);
-                const __m128i dq1 = _mm512_extracti32x4_epi32(packed, 1);
-                const __m128i dq2 = _mm512_extracti32x4_epi32(packed, 2);
-                const __m128i dq3 = _mm512_extracti32x4_epi32(packed, 3);
-
-                const __m128i shuffle = _mm_setr_epi8(
-                     0,  1,  2,
-                     4,  5,  6,
-                     8,  9, 10,
-                    12, 13, 14,
-                    char(0xff), char(0xff), char(0xff), char(0xff)
-                );
-
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 0*12), _mm_shuffle_epi8(dq0, shuffle));
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 1*12), _mm_shuffle_epi8(dq1, shuffle));
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 2*12), _mm_shuffle_epi8(dq2, shuffle));
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 3*12), _mm_shuffle_epi8(dq3, shuffle));
-#endif
-
-#ifdef AVX2_ASSISTED_STORE
-                const __m256i v0 = _mm512_extracti64x4_epi64(packed, 0);
-                const __m256i v1 = _mm512_extracti64x4_epi64(packed, 1);
-
-                const __m256i shuf = _mm256_setr_epi8(
-                       0,  1,  2,
-                       4,  5,  6,
-                       8,  9, 10,
-                      12, 13, 14,
-                      char(0xff), char(0xff), char(0xff), char(0xff),
-                       0,  1,  2,
-                       4,  5,  6,
-                       8,  9, 10,
-                      12, 13, 14,
-                      char(0xff), char(0xff), char(0xff), char(0xff)
-                );
-
-                const __m256i v0s = _mm256_shuffle_epi8(v0, shuf);
-                const __m256i v1s = _mm256_shuffle_epi8(v1, shuf);
-
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 0*12), _mm256_extracti128_si256(v0s, 0));
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 1*12), _mm256_extracti128_si256(v0s, 1));
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 2*12), _mm256_extracti128_si256(v1s, 0));
-                _mm_storeu_si128(reinterpret_cast<__m128i*>(out + 3*12), _mm256_extracti128_si256(v1s, 1));
-#endif
                 out += 48;
             }
         }
