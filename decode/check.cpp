265ee630b4d8401ecbe562841fe3032f9644d41d
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-10-26 18:25:56 +0200

    Move common includes to a helper file

--- decode/check.cpp
+++ decode/check.cpp
@@ -7,29 +7,7 @@
 #include "../cmdline.cpp"
 #include "../fnv32.cpp"
 
-#include "decode.common.cpp"
-#include "decode.scalar.cpp"
-#if defined(HAVE_SSE_INSTRUCTIONS)
-#   include "decoders.sse.cpp"
-#endif
-#if defined(HAVE_XOP_INSTRUCTIONS)
-#   include "decoders.xop.cpp"
-#endif
-#if defined(HAVE_AVX2_INSTRUCTIONS)
-#   include "decoders.avx2.cpp"
-#endif
-#if defined(HAVE_AVX512_INSTRUCTIONS)
-#   include "decoders.avx512.cpp"
-#endif
-#if defined(HAVE_AVX512BW_INSTRUCTIONS)
-#   include "decoders.avx512bw.cpp"
-#endif
-#if defined(HAVE_AVX512VBMI_INSTRUCTIONS)
-#   include "decoders.avx512vbmi.cpp"
-#endif
-#if defined(HAVE_NEON_INSTRUCTIONS)
-#   include "decoders.neon.cpp"
-#endif
+#include "all.cpp"
 
 #include "function_registry.cpp"
 #include "application.cpp"
--- decode/verify.cpp
+++ decode/verify.cpp
@@ -3,29 +3,7 @@
 
 #include "config.h"
 
-#include "decode.common.cpp"
-#include "decode.scalar.cpp"
-#if defined(HAVE_SSE_INSTRUCTIONS)
-#   include "decoders.sse.cpp"
-#endif
-#if defined(HAVE_XOP_INSTRUCTIONS)
-#   include "decoders.xop.cpp"
-#endif
-#if defined(HAVE_AVX2_INSTRUCTIONS)
-#   include "decoders.avx2.cpp"
-#endif
-#if defined(HAVE_AVX512_INSTRUCTIONS)
-#   include "decoders.avx512.cpp"
-#endif
-#if defined(HAVE_AVX512BW_INSTRUCTIONS)
-#   include "decoders.avx512bw.cpp"
-#endif
-#if defined(HAVE_AVX512VBMI_INSTRUCTIONS)
-#   include "decoders.avx512vbmi.cpp"
-#endif
-#if defined(HAVE_NEON_INSTRUCTIONS)
-#   include "decoders.neon.cpp"
-#endif
+#include "all.cpp"
 
 #include "function_registry.cpp"
 
