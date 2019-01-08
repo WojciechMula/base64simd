08eaa74195fc486f35937bbbca9ed743fddc916e
Daniel Lemire <lemire@gmail.com>
2019-01-08 15:52:50 -0500

    Cleaner memcpy

--- decode/application.cpp
+++ decode/application.cpp
@@ -8,10 +8,24 @@
 #include <immintrin.h>
 void* avx512_memcpy(void *dst, const void * src, size_t n) {
   if(n >= 64) {
-    size_t nminus64 = n - 64;
-    for(size_t i = 0; i <= nminus64; i+=64) {
-      __m512i x = _mm512_loadu_si512((const char*)src + i);
-      _mm512_storeu_si512((char*)dst + i, x);
+    size_t i = 0;
+    if(n >= 4*64) {
+      for(; i <= n - 4*64; i+=4*64) {
+        __m512i x0 = _mm512_loadu_si512((const char*)src + i);
+        __m512i x1 = _mm512_loadu_si512((const char*)src + i + 64);
+        __m512i x2 = _mm512_loadu_si512((const char*)src + i + 128);
+        __m512i x3 = _mm512_loadu_si512((const char*)src + i + 192);
+        _mm512_storeu_si512((char*)dst + i, x0);
+        _mm512_storeu_si512((char*)dst + i + 64, x1);
+        _mm512_storeu_si512((char*)dst + i + 128, x2);
+        _mm512_storeu_si512((char*)dst + i + 192, x3);
+     }
+    }
+    if(n>=64) {
+      for(; i <= n - 64; i+=64) {
+        __m512i x0 = _mm512_loadu_si512((const char*)src + i);
+        _mm512_storeu_si512((char*)dst + i, x0);
+      }
     }
     size_t leftover = n % 64;
     ::memcpy((char*)dst + n - leftover, (const char*)src + n - leftover, leftover);
